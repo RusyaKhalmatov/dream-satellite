97e443a3219b85963b381cfba1408b978481dd9c
RusyaKhalmatov <rusya.khalmatov@gmail.com>
2017-05-09 20:03:03 +0500

    Add files via upload

---
+++ news/tmp_news.php
@@ -0,0 +1,36 @@
+<?php 
+$news_id=$_GET["id"];
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+	<head>
+    <title>News</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Contact page dreamsatellite"/>
+    <link href="../css/bootstrap.css" rel="stylesheet">
+      <link href="../css/foorTempStyle.css" style="text/css" rel="stylesheet">
+      <link href="../css/contactStyle.css" rel="stylesheet" style="text/style">
+       
+	</head>
+	
+	<body>
+		  
+		    <!--Navigation side-->
+		    
+			<?php include("../user_page/user_navigation.php")?>
+			<!--Content side-->
+			
+			
+		
+			
+			<!--Foot side-->
+		<?php include("../scripts/foot_temp.php") ?>
+		
+	</body>
+
+</html>
